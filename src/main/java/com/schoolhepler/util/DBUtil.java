deadf3578780ee314dd0c6267cedd9c59ece8972
sakura-fly <sourcesakura@sina.com>
2019-01-14 17:42:23 +0800

    修改工具类

--- src/main/java/com/schoolhepler/util/DBUtil.java
+++ src/main/java/com/schoolhepler/util/DBUtil.java
@@ -9,9 +9,20 @@ import java.util.List;
 
 public class DBUtil {
 
-    public static NativeQuery getQuerySql(Session session, String table, Object o) {
+    public static NativeQuery getQuerySql(Session session, String table, Object o, String... zd) {
         List<SensorData> rre = RrelationData.sensorDataList(o);
-        String sqlStr = "select * from " + table + getQuerySql(rre);
+        StringBuilder zds = new StringBuilder();
+        if (zd.length == 0){
+            zds = new StringBuilder("*");
+        } else {
+            for (int i = 0; i < zd.length; i++){
+                zds.append(zd[i]);
+                if (i < zd.length - 1){
+                    zds.append(", ");
+                }
+            }
+        }
+        String sqlStr = "select " + zds.toString() + " from " + table + getQuerySql(rre);
         return getQuerySql(sqlStr, session, rre);
     }
 
@@ -20,8 +31,23 @@ public class DBUtil {
         if (rre.size() == 0) {
             return "";
         }
-        for (SensorData s : rre) {
-            sb.append(s.getSensorId()).append(" = ? ");
+        for (int i = 0; i < rre.size(); i++) {
+            sb.append(rre.get(i).getSensorId()).append(" = ? ");
+            if (i < rre.size() - 1)
+                sb.append(" and ");
+        }
+        return sb.toString();
+    }
+
+    public static String getQuerySqlByOr(List<SensorData> rre) {
+        StringBuilder sb = new StringBuilder(" where ");
+        if (rre.size() == 0) {
+            return "";
+        }
+        for (int i = 0; i < rre.size(); i++) {
+            sb.append(rre.get(i).getSensorId()).append(" = ? ");
+            if (i < rre.size() - 1)
+                sb.append(" or ");
         }
         return sb.toString();
     }
@@ -33,6 +59,15 @@ public class DBUtil {
         }
         return sql;
     }
+
+    public static NativeQuery getQuerySqlByLike(String q, Session session, List<SensorData> rre) {
+        NativeQuery sql = session.createSQLQuery(q);
+        for (int i = 0; i < rre.size(); i++) {
+            sql.setParameter(i + 1, "%" + rre.get(i).getSensorValue() + "%");
+        }
+        return sql;
+    }
+
 
 
 
